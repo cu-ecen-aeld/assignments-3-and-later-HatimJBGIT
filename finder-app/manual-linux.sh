a859949d8ae93ab141e0dd8ab6eebe224ef4f04c
HatimJB <hatimjb@Ubuntu.myguest.virtualbox.org>
2024-08-13 13:11:49 +0530

    assignment 3 part 2 1st commit

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -35,6 +35,16 @@ if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     git checkout ${KERNEL_VERSION}
 
     # TODO: Add your kernel build steps here
+    echo "[DEBUG] 1 configuring mproper"
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    echo "[DEBUG] 1 configuring defconfig"
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    echo "[DEBUG] 1 configuring all"
+    make -j 4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+# make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+    echo "[DEBUG] 1 configuring dtbs"
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
+    echo "[DEBUG] 2 configuring dtbs"
 fi
 
 echo "Adding the Image in outdir"
@@ -48,6 +58,10 @@ then
 fi
 
 # TODO: Create necessary base directories
+mkdir -p rootfs; cd rootfs
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
@@ -56,25 +70,54 @@ git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
     # TODO:  Configure busybox
+    make distclean
+    make defconfig
 else
     cd busybox
 fi
 
 # TODO: Make and install busybox
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+    make CONFIG_PREFIX="${OUTDIR}/rootfs" ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+    cd ${OUTDIR}/rootfs
 
 echo "Library dependencies"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
 # TODO: Add library dependencies to rootfs
+LIBC_PATH=~/cross_compiler/arm-cross-compiler/gcc-arm-10.3-2021.07-x86_64-aarch64-none-linux-gnu/aarch64-none-linux-gnu/libc
+
+cp ${LIBC_PATH}/lib/ld-linux-aarch64.so.1 lib
+cp ${LIBC_PATH}/lib64/libm.so.6 lib64
+cp ${LIBC_PATH}/lib64/libresolv.so.2 lib64
+cp ${LIBC_PATH}/lib64/libc.so.6 lib64
+
 
 # TODO: Make device nodes
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 666 dev/console c 5 1
 
 # TODO: Clean and build the writer utility
+ASSIGN_2_FILES=/home/hatimjb/course/assignment-2-HatimJBGIT/finder-app
+
+cd ${ASSIGN_2_FILES}
+make clean
+make $CROSS_COMPILE=aarch64-none-linux-gnu- all
+cp ${ASSIGN_2_FILES}/writer $OUTDIR/rootfs/home
 
 # TODO: Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp ${ASSIGN_2_FILES}/finder.sh $OUTDIR/rootfs/home
+cp ${ASSIGN_2_FILES}/finder-test.sh $OUTDIR/rootfs/home
+cp ${ASSIGN_2_FILES}/writer.sh $OUTDIR/rootfs/home
+cp -r ${ASSIGN_2_FILES}/conf/ $OUTDIR/rootfs/home
+cp /home/hatimjb/course/assignments-3-and-later-HatimJBGIT/finder-app/autorun-qemu.sh $OUTDIR/rootfs/home
 
 # TODO: Chown the root directory
 
 # TODO: Create initramfs.cpio.gz
+cd "$OUTDIR/rootfs"
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+gzip -f ${OUTDIR}/initramfs.cpio
+
